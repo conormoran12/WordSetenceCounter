7c3fac66b1d97c6a7ae38b1e481e6db4028c015e
Conor Moran <{ID}+{username}@users.noreply.github.com>
2023-09-12 15:59:59 +0100

    Add project files.

---
+++ WordSetenceCounter/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace WordSetenceCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a paragraph:");
+            string userInput = Console.ReadLine();
+            Console.WriteLine($"Characters: {userInput.Length}");
+            Console.WriteLine($"Words: {userInput.Split(" ").Length}");
+            Console.WriteLine($"Sentences: {userInput.Split(". ").Length}");
+        }
+    }
+}
